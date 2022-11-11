c04c5e2f6e974d49bceef25b2edd5fb8e6dee005
imdarsh <shindesahil63@gmail.com>
2022-11-11 18:40:54 +0530

    Complete react redux counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({type: 'INC'})
+  }
+
+  const decrement = () => {
+    dispatch({ type: 'DEC' })
+  }
+
+  const addBy = () => {
+    dispatch({ type: 'ADD', payload: 10 })
+  }
+
+  const subBy = () => {
+    dispatch({ type: 'SUB', payload: 10 })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Counter App</h2>
+      <h2>{counter}</h2>
+      <button onClick={addBy}>Add By 10</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={subBy}>Sub By 10</button>
     </div>
   );
 }
